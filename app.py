4aea4cafcb69c6bdbf344d20ae10dfe4a969bfee
jane20030605 <yijanelin2@gmail.com>
2024-11-04 20:29:48 +0800

    1104sql

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import sqlite3
 
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('sample.db')
 cursor = conn.cursor()
 
 cursor.execute("SELECT * FROM customer")
 data = cursor.fetchall()
+print(type(data))
 for rec in data:
     print(f"ID：{rec[0]}, Name：{rec[1]}, Tel：{rec[2]}, Addr：{rec[3]}")
 
-cursor.execute("INSERT INTO customer(cid, cname, ctel, cadd) VALUES (?, ?, ?, ?)", ('c004', 'Cathy', '0937-543889', 'Tainan City daye road'))
+# ursor.execute("INSERT INTO customer(cid, cname, ctel, cadd) VALUES (?, ?, ?, ?)", ('c004', 'Cathy', '0937-543889', 'Tainan City daye road'))
 conn.commit()
 
 cursor.close()
